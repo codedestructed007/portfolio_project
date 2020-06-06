ab4eb54622273d3a42beb1a26b045f49012c4ba7
Satyam Sharma <55931484+codedestructed007@users.noreply.github.com>
2020-06-06 12:38:56 +0530

    i commented on line no 91

--- New folder/portfolio/settings.py
+++ New folder/portfolio/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     }
 }
 
-
+# just a test to cheak pull is working correctly or not
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
